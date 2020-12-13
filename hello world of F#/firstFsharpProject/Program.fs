daa1e3717ba3f87e8d12ed05d9253ae44dee6f68
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-12-13 23:36:57 +0600

    hello world of f#

---
+++ hello world of F#/firstFsharpProject/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let firstHelloWorld (name: string) =
+    
+    "Hello world, my name is " + string(name)
+
+[<EntryPoint>]
+let main argv =
+    for name in argv do
+        
+        printfn "Hello World from F#!"
+        let ret = firstHelloWorld name
+        printfn "%s" ret
+    0 // return an integer exit code
---
+++ hello world of F#/firstFsharpProject/hello_world.fsx
@@ -0,0 +1,3 @@
+let firstHelloWorld (name: string) =
+    
+    "Hello world, my name is " + string(name)
