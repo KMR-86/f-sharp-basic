09c1a96ff43fefbb1375c887852a23ff29a43e59
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-12-17 00:25:17 +0600

    eular problem 2 is solved

---
+++ classical problems solved with f#/classical_problems/Program.fs
@@ -0,0 +1,56 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+module problems =
+    let ar = Array.create 100 0
+    let rec fibo_naive x=
+        if x < 3 then 
+            1
+        else 
+            fibo_naive (x-1) + fibo_naive (x-2)
+            
+    let rec fibo x=
+        if ar.[x]<>0 then ar.[x]
+        else
+            if x < 3 then 
+                ar.[x]<-1
+                ar.[x]
+            else 
+                ar.[x]<-fibo (x-1) + fibo (x-2)
+                ar.[x]
+    
+    let fibo_sum x =
+        let fibo_seq=(fibo x)
+        let mutable sum = 0
+        for el in ar do
+            if el < 4000000 && el%2=0 then sum<-sum+el
+            else sum<-sum+0
+        
+        sum
+
+    let fib_sum_iter lim =
+         let fib_ar = Array.create 100 0
+         fib_ar.[0]<-1
+         fib_ar.[1]<-1
+         let mutable fibsum = 0
+         for i in 2..40 do
+            fib_ar.[i] <- fib_ar.[i-1] + fib_ar.[i-2]
+            if fib_ar.[i] < lim && fib_ar.[i] % 2=0 then fibsum <- fibsum + fib_ar.[i]
+            
+         fibsum
+
+[<EntryPoint>]
+let main argv =
+    
+    printfn "%d" (problems.fibo 45)
+    printfn "%d" (problems.fibo_sum 45)
+    printfn "%d" (problems.fib_sum_iter 4000000)
+    ///printfn "%d" (problems.fibo_naive 45)
+
+
+    0 // return an integer exit code
--- eular project solutions/eularProjectSolutions/Program.fs
+++ eular project solutions/eularProjectSolutions/Program.fs
@@ -42,13 +42,53 @@ module eular_project=
         let sum = get_sum x
         printfn "eular problem 1 the sum is %i" sum
     //prob1 1000
+
+    let rec fibo x=
+        //printfn "%i" x
+        if x < 3 then 1
+        else fibo (x-1) + fibo (x-2)
+    
+
+module eular_problem_2=
+    let ar = Array.create 100 0
+    let rec fibo x=
+        if ar.[x]<>0 then ar.[x]
+        else
+            if x < 3 then 
+                ar.[x]<-1
+                ar.[x]
+            else 
+                ar.[x]<-fibo (x-1) + fibo (x-2)
+                ar.[x]
+    
+    let fibo_sum x =
+        let fibo_seq=(fibo x)
+        let mutable sum = 0
+        for el in ar do
+            if el < 4000000 && el%2=0 then sum<-sum+el
+            else sum<-sum+0
+        
+        sum
+
+    let fibo_sum_iter lim =
+         let fib_ar = Array.create 100 0
+         fib_ar.[0]<-1
+         fib_ar.[1]<-1
+         let mutable fibsum = 0
+         for i in 2..40 do
+            fib_ar.[i] <- fib_ar.[i-1] + fib_ar.[i-2]
+            if fib_ar.[i] < lim && fib_ar.[i] % 2=0 then fibsum <- fibsum + fib_ar.[i]
+            
+         fibsum
+
+
 [<EntryPoint>]
 let main argv =
 
-    eular_project.prob1_basic 1000
-    eular_project.prob1_imp1 1000
-    eular_project.prob1_imp2 1000
-    eular_project.prob1_imp3 1000
-    eular_project.prob1_imp4 1000
+    printfn "%d" (eular_problem_2.fibo_sum 45)
+    printfn "%d" (eular_problem_2.fibo_sum_iter 4000000)
+    
+
+    
     
     0 // return an integer exit code
